ac2e3a4e3f680956c14986d2886c488ed79a399a
Omikhe <msakamike84@gmail.com>
2025-07-31 14:32:21 +0200

    fix scaling issue

--- app/components/sidenav.tsx
+++ app/components/sidenav.tsx
@@ -1,35 +1,72 @@
-import { socialLinks, navLinks } from '@/app/components/linkz'
-import Link from 'next/link';
+"use client"
 
-export default function sidenav() {
+import { socialLinks, navLinks } from "@/app/components/linkz";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import Image from "next/image";
+
+export default function Sidenav() {
+  const pathname = usePathname();
+
   return (
-    <div className="mx-40 mt-24 border">
-      <div className=" flex flex-col gap-4 w-[82px]">
-
-      <div className="size-20 bg-[#272727] rounded-full"></div>
-
-      <div className="">
-        <div className="p-2 bg-[#161616] border-[#202020] border-2 w-auto rounded-full">
-          <div className="flex flex-col w-auto gap-5  bg-gradient-to-b from-[#282828] to-[#1B1B1B] rounded-full">
-            {navLinks.map((link) => (
-                          <div className=" p-4 h-auto w-auto flex justify-center items-center hover:bg-gradient-to-b from-[#545454] to-[#2F2F2F] rounded-full">
-                            <Link
-                                key={link.href}
-                                href={link.href}
-                                className=" rounded-full h-auto w-auto"
-                            >
-                              
-                                    {link.icon}
-                              
-                                
-                            </Link>
-                            </div>
-                        ))}
-            
-          </div>
+    <div className="mx-28 flex flex-col justify-center min-h-screen">
+      <div className="flex flex-col gap-14 w-[82px] items-center justify-center">
+        {/* Profile/Avatar */}
+        <div className="size-16 bg-[#ffffff] rounded-full overflow-hidden flex items-center justify-center">
+          <Image
+            width={55}
+            height={55}
+            src="/omikhe-avatar.png"
+            alt="Mike Msaka - Graphic Designer"
+            priority
+            className="rounded-full"
+          />
         </div>
-      </div>
+        
+         {/* Navigation Links */}
+        <nav>
+          <div className="p-1 bg-[#161616] border-[#202020] border-2 rounded-full">
+            <div className="flex flex-col gap-2 bg-gradient-to-b from-[#282828] to-[#1B1B1B] rounded-full">
+              {navLinks.map((link) => {
+                const isActive = pathname === link.href;
+                return (
+                  <div key={link.href} className="relative group">
+                    <Link
+                      href={link.href}
+                      className={`m-1 p-2 flex justify-center items-center rounded-full transition-all duration-200 ${
+                        isActive
+                          ? "text-[#F2F2F2] bg-gradient-to-b from-[#545454] to-[#2F2F2F] shadow-md"
+                          : "text-[#565656] hover:text-[#F2F2F2] hover:bg-gradient-to-b from-[#545454] to-[#2F2F2F] hover:shadow-md"
+                      }`}
+                    >
+                      {link.icon}
+                    </Link>
+                    
+                    {/* Tooltip */}
+                    <div className="absolute left-full ml-3 top-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none">
+                      <div className="bg-gradient-to-b from-[#545454] to-[#2F2F2F] text-[#F2F2F2] px-3 py-2 rounded-full text-sm whitespace-nowrap shadow-lg">
+                        {link.label}
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </nav>
 
+        {/* Social Links */}
+        <div className="flex flex-col gap-2">
+          {socialLinks.map(({ href, icon }) => (
+            <Link
+              key={href}
+              href={href}
+              className="text-[#565656] hover:text-[#F2F2F2] transition-colors"
+            >
+              {icon}
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
