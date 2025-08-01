3118599fd367c0c117d32f5ae54b92d55e294e53
Omikhe <msakamike84@gmail.com>
2025-08-02 01:41:11 +0200

    change scroll behaviour

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Mike Msaka",
-  description: "Mike Msaka's portifolio",
+  description: "Mike Msaka's portfolio",
 };
 
 export default function RootLayout({
@@ -25,14 +25,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
-        <div className="flex min-h-screen">
-          {/* Sidenav - Fixed on the left side */}
-          <Sidenav />
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <div className="flex h-screen overflow-hidden">
+          {/* Fixed Sidenav */}
+          <aside className="flex-shrink-0">
+            <Sidenav />
+          </aside>
           
-          {/* Main content area */}
-          <main className="flex-1">
-            {children}
+          {/* Scrollable Main Content Area */}
+          <main className="flex-1 overflow-y-auto overflow-x-hidden">
+            <div className="min-h-full">
+              {children}
+            </div>
           </main>
         </div>
       </body>
