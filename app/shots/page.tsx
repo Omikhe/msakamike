c0dd30ce3d16e672785344299c22324c28f5ed20
Omikhe <msakamike84@gmail.com>
2025-08-02 23:24:03 +0200

    add placeholder

---
+++ app/shots/page.tsx
@@ -0,0 +1,9 @@
+import { Camera } from "lucide-react"
+export default function Shots() {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <Camera size={300} className="-z-0 text-[#1f1f1f]"></Camera>
+
+        </div>
+    )
+}
