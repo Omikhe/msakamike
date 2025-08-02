44c9149f300368bc5b4a1f86976a29ec0403d789
Omikhe <msakamike84@gmail.com>
2025-08-02 22:51:47 +0200

    add responsiveness

--- app/design/page.tsx
+++ app/design/page.tsx
@@ -6,7 +6,7 @@ const categories = [
 ]
 export default function Design() {
     return (
-        <div className="mt-10 flex flex-col gap-8">
+        <div className="lg:mx-0 mx-4 mt-10 flex flex-col gap-8">
             <div className="flex gap-2">
                 {categories.map((category) => (
                     <div
@@ -17,9 +17,10 @@ export default function Design() {
                 ))}
             </div>
 
-            <div className="flex flex-wrap gap-6 mr-16">
+            <div className="flex flex-wrap gap-4 lg:gap-6 lg:mr-16 overflow-hidden">
+
                 <div className="flex flex-col gap-1">
-                    <div className="h-[300px] w-[300px] rounded-3xl bg-[#272727] font-mono"></div>
+                    <div className="h-[358px] w-[358px] lg:h-[300px] lg:w-[300px] rounded-3xl bg-[#272727] font-mono"></div>
                     <div className="flex gap-2 hover:underline">
                         <h3 className="font-mono ml-1.5 ">Valuations Africa</h3>
                         <ArrowUpRight></ArrowUpRight>
@@ -27,7 +28,7 @@ export default function Design() {
                 </div>
 
                 <div className="flex flex-col gap-1">
-                    <div className="h-[300px] w-[300px] rounded-3xl bg-[#272727] font-mono"></div>
+                    <div className="h-[358px] w-[358px] lg:h-[300px] lg:w-[300px] rounded-3xl bg-[#272727] font-mono"></div>
                     <div className="flex gap-2 hover:underline">
                         <h3 className="font-mono ml-1.5 ">Valuations Africa</h3>
                         <ArrowUpRight></ArrowUpRight>
@@ -35,7 +36,7 @@ export default function Design() {
                 </div>
 
                 <div className="flex flex-col gap-1">
-                    <div className="h-[300px] w-[300px] rounded-3xl bg-[#272727] font-mono"></div>
+                    <div className="h-[358px] w-[358px] lg:h-[300px] lg:w-[300px] rounded-3xl bg-[#272727] font-mono"></div>
                     <div className="flex gap-2 hover:underline">
                         <h3 className="font-mono ml-1.5 ">Valuations Africa</h3>
                         <ArrowUpRight></ArrowUpRight>
@@ -43,36 +44,13 @@ export default function Design() {
                 </div>
 
                 <div className="flex flex-col gap-1">
-                    <div className="h-[300px] w-[300px] rounded-3xl bg-[#272727] font-mono"></div>
+                    <div className="h-[358px] w-[358px] lg:h-[300px] lg:w-[300px] rounded-3xl bg-[#272727] font-mono"></div>
                     <div className="flex gap-2 hover:underline">
                         <h3 className="font-mono ml-1.5 ">Valuations Africa</h3>
                         <ArrowUpRight></ArrowUpRight>
                     </div>
                 </div>
 
-                <div className="flex flex-col gap-1">
-                    <div className="h-[300px] w-[300px] rounded-3xl bg-[#272727] font-mono"></div>
-                    <div className="flex gap-2 hover:underline">
-                        <h3 className="font-mono ml-1.5 ">Valuations Africa</h3>
-                        <ArrowUpRight></ArrowUpRight>
-                    </div>
-                </div>
-
-                <div className="flex flex-col gap-1">
-                    <div className="h-[300px] w-[300px] rounded-3xl bg-[#272727] font-mono"></div>
-                    <div className="flex gap-2 hover:underline">
-                        <h3 className="font-mono ml-1.5 ">Valuations Africa</h3>
-                        <ArrowUpRight></ArrowUpRight>
-                    </div>
-                </div>
-
-                <div className="flex flex-col gap-1">
-                    <div className="h-[300px] w-[300px] rounded-3xl bg-[#272727] font-mono"></div>
-                    <div className="flex gap-2 hover:underline">
-                        <h3 className="font-mono ml-1.5 ">Valuations Africa</h3>
-                        <ArrowUpRight></ArrowUpRight>
-                    </div>
-                </div>
 
 
 
