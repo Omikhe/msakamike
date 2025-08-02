80d008fe72a33d893bd1f7d0be31a862841859a3
Omikhe <msakamike84@gmail.com>
2025-08-02 23:20:54 +0200

    add placeholder

---
+++ app/code/page.tsx
@@ -0,0 +1,9 @@
+import { Code } from "lucide-react"
+export default function About() {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <Code size={300} className="-z-0 text-[#1f1f1f]"></Code>
+
+        </div>
+    )
+}
